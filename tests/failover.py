00b742a8e0de1f26a4983df42e807ef021b32460
Dudi Zimberknopf <dudi@vizo.dev>
2024-03-18 12:58:06 +0200

    add data check in test

--- tests/failover.py
+++ tests/failover.py
@@ -1,45 +1,74 @@
+import sys
 import redis
 from redis.sentinel import Sentinel
-from time import sleep
+from time import sleep, time
 
-SENTINEL_HOST = (
-    "r-q3fscdg4lc.instance-t1mjddzwl.hc-uxl2s51wd.us-east1.gcp.f2e0a955bb84.cloud"
-)
-SENTINEL_PORT = "26379"
-ADMIN_PASSWORD = "rZyD2N7UsFwzkb"
+SENTINEL_HOST = sys.argv[1] if len(sys.argv) > 1 else "localhost"
+SENTINEL_PORT = int(sys.argv[2]) if len(sys.argv) > 2 else 26379
+ADMIN_PASSWORD = sys.argv[3] if len(sys.argv) > 3 else "admin"
 
-SLEEP_SECONDS = 2
 MAX_FAILOVER_TIME_SECONDS = 10
 
 
 def test_failover():
 
-    sentinel = Sentinel([(SENTINEL_HOST, SENTINEL_PORT)], sentinel_kwargs={"password": ADMIN_PASSWORD}, connection_kwargs={"password": ADMIN_PASSWORD})
+    sentinel = Sentinel(
+        [(SENTINEL_HOST, SENTINEL_PORT)],
+        sentinel_kwargs={"password": ADMIN_PASSWORD},
+        connection_kwargs={"password": ADMIN_PASSWORD},
+    )
 
     retries = 0
+    last_time = 0
+    failover_triggered_time = 0
 
     while True:
         try:
             master = sentinel.discover_master("master")
+            if (master is None) or (len(master) < 2):
+                print("No master found")
+                break
             r = redis.StrictRedis(
                 host=master[0], port=master[1], password=ADMIN_PASSWORD, db=0
             )
-            r.set("foo", "bar")
-            assert r.get("foo") == b"bar"
 
             if retries > 0:
-                print("Failover successful")
+                if r.get("time") != str(last_time).encode("utf-8"):
+                    print(
+                        "Data lost: "
+                        + str(r.get("time"))
+                        + " != "
+                        + str(last_time)
+                        + " retries: "
+                        + str(retries)
+                    )
+                    break
+                print(
+                    "Failover successful. Took " + str(time() - last_time) + " seconds"
+                )
                 break
-
-            sleep(SLEEP_SECONDS)
-        except:
-            print("Could not connect")
+            
+            tmp = last_time
+            last_time = time()
+            try:
+                r.set("time", last_time)
+            except Exception as e:
+                last_time = tmp
+                raise e
+            if r.get("time") != str(last_time).encode("utf-8"):
+                print("Data lost: " + str(r.get("time")) + " != " + str(last_time))
+                break
+            sleep(0.5)
+        except Exception as e:
+            print("Could not connect", e)
+            if failover_triggered_time == 0:
+                failover_triggered_time = time()
             retries += 1
-            if retries > MAX_FAILOVER_TIME_SECONDS / SLEEP_SECONDS:
-                raise Exception("Failover took too long")
+            if (time() - failover_triggered_time) > MAX_FAILOVER_TIME_SECONDS:
+                print("Failover took too long: " + str(time() - failover_triggered_time))
+            sleep(0.5)
             pass
-
+        
 
 if __name__ == "__main__":
     test_failover()
-    print("Failover test passed")

ba5190dc1b22390c048fd99636865bc6bf6bec0b
Dudi Zimberknopf <zimber.dudi@gmail.com>
2024-07-24 09:55:57 +0300

    add tests for cluster

---
+++ omnistrate_tests/test_cluster.py
@@ -0,0 +1,176 @@
+import sys
+from pathlib import Path # if you haven't already done so
+file = Path(__file__).resolve()
+parent, root = file.parent, file.parents[1]
+sys.path.append(str(root))
+
+# Additionally remove the current file's directory from sys.path
+try:
+    sys.path.remove(str(parent))
+except ValueError: # Already removed
+    pass
+
+import time
+import os
+from omnistrate_tests.classes.omnistrate_fleet_instance import OmnistrateFleetInstance
+from omnistrate_tests.classes.omnistrate_fleet_api import OmnistrateFleetAPI
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument("omnistrate_user")
+parser.add_argument("omnistrate_password")
+parser.add_argument("cloud_provider", choices=["aws", "gcp"])
+parser.add_argument("region")
+
+parser.add_argument(
+    "--subscription-id", required=False, default=os.getenv("SUBSCRIPTION_ID")
+)
+parser.add_argument("--ref-name", required=False, default=os.getenv("REF_NAME"))
+parser.add_argument("--service-id", required=True)
+parser.add_argument("--environment-id", required=True)
+parser.add_argument("--resource-key", required=True)
+
+
+parser.add_argument("--instance-name", required=True)
+parser.add_argument("--instance-description", required=False, default="test-standalone")
+parser.add_argument("--instance-type", required=True)
+parser.add_argument("--storage-size", required=False, default="30")
+parser.add_argument("--tls", action="store_true")
+parser.add_argument("--rdb-config", required=False, default="medium")
+parser.add_argument("--aof-config", required=False, default="always")
+parser.add_argument("--host-count", required=False, default="6")
+parser.add_argument("--cluster-replicas", required=False, default="1")
+
+parser.set_defaults(tls=False)
+args = parser.parse_args()
+
+
+def test_standalone():
+
+    omnistrate = OmnistrateFleetAPI(
+        email=args.omnistrate_user,
+        password=args.omnistrate_password,
+    )
+
+    service = omnistrate.get_service(args.service_id)
+    product_tier = omnistrate.get_product_tier(
+        service_id=args.service_id,
+        environment_id=args.environment_id,
+        tier_name=args.ref_name,
+    )
+    service_model = omnistrate.get_service_model(
+        args.service_id, product_tier.service_model_id
+    )
+
+    print(f"Product tier id: {product_tier.product_tier_id} for {args.ref_name}")
+
+    instance = omnistrate.instance(
+        service_id=args.service_id,
+        service_provider_id=service.service_provider_id,
+        service_key=service.key,
+        service_environment_id=args.environment_id,
+        service_environment_key=service.get_environment(args.environment_id).key,
+        service_model_key=service_model.key,
+        service_api_version="v1",
+        product_tier_key=product_tier.product_tier_key,
+        resource_key=args.resource_key,
+        subscription_id=args.subscription_id,
+    )
+
+    try:
+        instance.create(
+            wait_for_ready=True,
+            deployment_cloud_provider=args.cloud_provider,
+            deployment_region=args.region,
+            name=args.instance_name,
+            description=args.instance_description,
+            falkordb_user="falkordb",
+            falkordb_password="falkordb",
+            nodeInstanceType=args.instance_type,
+            storageSize=args.storage_size,
+            enableTLS=args.tls,
+            RDBPersistenceConfig=args.rdb_config,
+            AOFPersistenceConfig=args.aof_config,
+        )
+
+        # Test failover and data loss
+        test_failover(instance)
+
+        # Test stop and start instance
+        test_stop_start(instance)
+    except Exception as e:
+        instance.delete(True)
+        raise e
+
+    # Delete instance
+    instance.delete(True)
+
+    print("Test passed")
+
+
+def test_failover(instance: OmnistrateFleetInstance):
+    """This function should retrieve the instance host and port for connection, write some data to the DB, then trigger a failover. After X seconds, the instance should be back online and data should have persisted"""
+
+    # Get instance host and port
+    db = instance.create_connection(
+        ssl=args.tls,
+    )
+
+    graph = db.select_graph("test")
+
+    # Write some data to the DB
+    graph.query("CREATE (n:Person {name: 'Alice'})")
+
+    # Trigger failover
+    instance.trigger_failover(
+        replica_id=args.replica_id,
+        wait_for_ready=True,
+    )
+
+    # Check if data is still there
+
+    graph = db.select_graph("test")
+
+    result = graph.query("MATCH (n:Person) RETURN n")
+
+    if len(result.result_set) == 0:
+        raise Exception("Data lost after failover")
+
+    print("Data persisted after failover")
+
+    graph.delete()
+
+
+def test_stop_start(instance: OmnistrateFleetInstance):
+    """This function should stop the instance, check that it is stopped, then start it again and check that it is running"""
+
+    # Get instance host and port
+    db = instance.create_connection(
+        ssl=args.tls,
+    )
+
+    graph = db.select_graph("test")
+
+    # Write some data to the DB
+    graph.query("CREATE (n:Person {name: 'Alice'})")
+
+    print("Stopping instance")
+
+    instance.stop(wait_for_ready=True)
+
+    print("Instance stopped")
+
+    instance.start(wait_for_ready=True)
+
+    graph = db.select_graph("test")
+
+    result = graph.query("MATCH (n:Person) RETURN n")
+
+    if len(result.result_set) == 0:
+        raise Exception("Data lost after stop/start")
+
+    print("Instance started")
+
+
+if __name__ == "__main__":
+    test_standalone()

150c019062a7b2146afd14b9dcbf6727cb85ec4c
muhammad <memomq70@gmail.com>
2024-09-16 12:16:25 +0300

    added a print statement to see the output of the failover command

--- omnistrate_tests/test_replication_replicas.py
+++ omnistrate_tests/test_replication_replicas.py
@@ -169,7 +169,8 @@ def test_fail_over(instance: OmnistrateFleetInstance):
         if time.time() > tout:
             raise Exception(f"Failed to failover to node-{id_key}-2")
         try:
-            client.execute_command('SENTINEL FAILOVER master')
+            f = client.execute_command('SENTINEL FAILOVER master')
+            print(f)
             time.sleep(10)
             master = client.execute_command('SENTINEL MASTER master')[3]
             if master.startswith(f"node-{id_key}-2"):

2f6f625ed8df9ab69facd52daf5e791687c4a53f
muhammad <memomq70@gmail.com>
2024-09-17 10:00:12 +0300

    Added threading to other tests

--- omnistrate_tests/test_cluster_shards.py
+++ omnistrate_tests/test_cluster_shards.py
@@ -2,6 +2,8 @@ import sys
 import signal
 from random import randbytes
 from pathlib import Path
+import threading
+
 
 file = Path(__file__).resolve()
 parent, root = file.parent, file.parents[1]
@@ -124,6 +126,14 @@ def test_cluster_shards():
 
         add_data(instance)
 
+        # Start a new thread and signal for zero_downtime test
+        thread_signal = threading.Event()
+        error_signal = threading.Event()
+        thread = threading.Thread(
+            target=test_zero_downtime, args=(thread_signal, error_signal, instance, args.tls)
+        )
+        thread.start()
+
         change_host_count(instance, int(args.host_count) + 2)
 
         if args.ensure_mz_distribution:
@@ -136,6 +146,10 @@ def test_cluster_shards():
         if args.ensure_mz_distribution:
             test_ensure_mz_distribution(instance, password)
 
+        # Wait for the zero_downtime
+        thread_signal.set()
+        thread.join()
+        
         check_data(instance)
     except Exception as e:
         logging.exception(e)
@@ -154,34 +168,8 @@ def change_host_count(instance: OmnistrateFleetInstance, new_host_count: int):
     logging.info(f"Changing host count to {new_host_count}")
     instance.update_params(
         hostCount=new_host_count,
-        wait_for_ready=False,
+        wait_for_ready=True,
     )
-
-    db = instance.create_connection(
-        ssl=args.tls,
-    )
-
-    graph = db.select_graph("test")
-
-    # Write some data to the DB
-    graph.query("CREATE (n:Person {name: 'Alice'})")
-    count = 0
-    time_out = time.time() + 1200
-
-    while True:
-        status = instance.get_instance_details()['status']
-
-        if time.time() > time_out:
-            raise Exception(f"Timeout occured after the instance state was in the {status} status for 20 minutes")
-        
-        if status == "DEPLOYING":
-            graph.query(f"CREATE (n:Person {{name: 'Alice{str(count)}'}})")
-            result = graph.query(f"MATCH (n:Person {{name: 'Alice{str(count)}'}}) RETURN n")
-        else:
-            break
-        count += 1
-
-    print("Zero downtime passed ")
 
     current_host_count = new_host_count
 
@@ -310,6 +298,31 @@ def check_data(instance: OmnistrateFleetInstance):
 
     if len(result.result_set) == 0:
         raise Exception("Data did not persist after host count change")
+    
+
+def test_zero_downtime(
+    thread_signal: threading.Event,
+    error_signal: threading.Event,
+    instance: OmnistrateFleetInstance,
+    ssl=False,
+):
+    """This function should test the ability to read and write while a memory update happens"""
+    try:
+        db = instance.create_connection(ssl=ssl, force_reconnect=True)
+
+        graph = db.select_graph("test")
+
+        while not thread_signal.is_set():
+            # Write some data to the DB
+            graph.query("CREATE (n:Person {name: 'Alice'})")
+            graph.ro_query("MATCH (n:Person {name: 'Alice'}) RETURN n")
+
+            time.sleep(3)
+    except Exception as e:
+        logging.exception(e)
+        error_signal.set()
+        raise e
+
     
 if __name__ == "__main__":
     test_cluster_shards()

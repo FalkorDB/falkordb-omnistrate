359b63fb1d526f4b8f08db7e76d3cdaa1b98324a
muhammad <memomq70@gmail.com>
2024-09-24 14:56:36 +0300

    set the cao to 30 but ileft retries at 30

--- omnistrate_tests/test_replication.py
+++ omnistrate_tests/test_replication.py
@@ -199,7 +199,7 @@ def test_failover(instance: OmnistrateFleetInstance, password: str):
         ssl=args.tls,
     )
 
-    retry = Retry(ExponentialBackoff(base=3,cap=20),20,supported_errors=(
+    retry = Retry(ExponentialBackoff(base=3,cap=30),30,supported_errors=(
         TimeoutError,
         ConnectionError,
         ConnectionRefusedError

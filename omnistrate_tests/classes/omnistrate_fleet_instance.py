6540855207cfd9c68f1b968a6e7b485997eed6a7
muhammad <memomq70@gmail.com>
2024-09-25 15:05:19 +0300

    changed dependency and set retry to negative 1 and cap=3

--- omnistrate_tests/classes/omnistrate_fleet_instance.py
+++ omnistrate_tests/classes/omnistrate_fleet_instance.py
@@ -528,7 +528,7 @@ class OmnistrateFleetInstance:
                     cluster_error_retry_attempts=20,
                     retry=retry.Retry(
                         retries=-1,
-                        backoff=backoff.ExponentialBackoff(base=3,cap=5),
+                        backoff=backoff.ExponentialBackoff(base=3,cap=3),
                         supported_errors=(
                             ConnectionRefusedError,
                             ConnectionError,

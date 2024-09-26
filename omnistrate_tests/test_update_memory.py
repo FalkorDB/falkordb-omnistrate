be5e430d6ceea5fb36ff4cc3153af9e34d617f81
muhammad <memomq70@gmail.com>
2024-09-26 13:21:31 +0300

    added print

--- omnistrate_tests/test_update_memory.py
+++ omnistrate_tests/test_update_memory.py
@@ -203,15 +203,14 @@ def test_zero_downtime(
             # Write some data to the DB
             try:
                 graph.query("CREATE (n:Person {name: 'Alice'})")
-            except:
+                graph.ro_query("MATCH (n:Person {name: 'Alice'}) RETURN n")
+            except (ConnectionError, ReadOnlyError) as e:
                 logging.info("THE CREATE COMMAND FAILED")
                 print("THE CREATE COMMAND FAILED")
                 db.connection.close()
                 db = instance.create_connection(ssl=ssl, force_reconnect=True)
                 graph = db.select_graph("test")
                 continue
-            graph.ro_query("MATCH (n:Person {name: 'Alice'}) RETURN n")
-
             time.sleep(3)
     except Exception as e:
         logging.exception(e)

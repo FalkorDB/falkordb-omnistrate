efe1b69bea8eed068c422f458791b6cfc89952e1
muhammad <memomq70@gmail.com>
2024-09-30 20:13:11 +0300

    increased sleep time

--- omnistrate_tests/test_replication.py
+++ omnistrate_tests/test_replication.py
@@ -389,7 +389,7 @@ def test_stop_start(instance: OmnistrateFleetInstance, password: str):
     instance.start(wait_for_ready=True)
     
     print("sleeping to see if it is a latency issue..")
-    time.sleep(60)
+    time.sleep(120)
     graph = db.select_graph("test")
     
     result = graph.query("MATCH (n:Person) RETURN n")

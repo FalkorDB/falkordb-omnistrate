7fbbfd4e5e8a5846d55b1db83319e8f677a2751c
muhammad <memomq70@gmail.com>
2024-10-21 09:58:47 +0300

    changed trap in entrypoint

--- falkordb-node/node-entrypoint.sh
+++ falkordb-node/node-entrypoint.sh
@@ -184,9 +184,8 @@ handle_sigterm() {
 }
 
 #trap handle_sigterm SIGTERM
-trap 'echo "Received a SIGTERM"' SIGTERM
-trap 'echo "Received a SIGKILL"' SIGKILL
-trap 'echo "Received a SIGINT"' SIGINT
+trap 'echo "$(date): received SIGTERM" >> /proc/1/fd/1' SIGTERM
+trap 'echo "$(date): received SIGINT" >> /proc/1/fd/1' SIGINT
 
 
 log() {

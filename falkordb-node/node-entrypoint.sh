4ae632ac57aee1a200044675807f67ac2514ea9c
muhammad <memomq70@gmail.com>
2024-10-22 13:43:26 +0300

    fixed if statement in node-entrypoint

--- falkordb-node/node-entrypoint.sh
+++ falkordb-node/node-entrypoint.sh
@@ -510,7 +510,7 @@ if [ -f /usr/local/bin/healthcheck ]; then
     healthcheck_pid=$!
   fi
 
-  if [[ $RUN_SENTINEL -eq 1 ]] && [[ $RUN_HEALTH_CHECK_SENTINEL -eq 1 ]]; then
+  if [[ $RUN_SENTINEL -eq 1 ]] && [[ $RUN_HEALTH_CHECK_SENTINEL -eq 1 ]] && [[ ("$HOSTNAME" =~ ^sentinel.*0 ) || ("$NODE_INDEX" == "1" || "$NODE_INDEX" == "0") ]]; then
     echo "Starting Sentinel Healthcheck"
     healthcheck sentinel &
     sentinel_healthcheck_pid=$!

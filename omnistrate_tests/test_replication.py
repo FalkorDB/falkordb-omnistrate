065b6c3af7143a8db26d0e9a9cb6935301e1eb34
muhammad <memomq70@gmail.com>
2024-11-10 09:58:52 +0200

    set the failover-timeout to 180000 and commented remove_master_from_group

--- omnistrate_tests/test_replication.py
+++ omnistrate_tests/test_replication.py
@@ -386,7 +386,6 @@ def test_failover(instance: OmnistrateFleetInstance, password: str,timeout_in_se
         raise Exception("Data lost after third failover")
 
     logging.info("Data persisted after third failover")
-    time.sleep(60)
 
 def test_stop_start(instance: OmnistrateFleetInstance, password: str):
     """

ef9171093a61cdf287be8c92c735e968cb6c1154
muhammad <memomq70@gmail.com>
2024-11-12 16:14:41 +0200

    retest

--- omnistrate_tests/test_replication.py
+++ omnistrate_tests/test_replication.py
@@ -387,6 +387,7 @@ def test_failover(instance: OmnistrateFleetInstance, password: str,timeout_in_se
 
     logging.info("Data persisted after third failover")
     time.sleep(180)
+    
 def test_stop_start(instance: OmnistrateFleetInstance, password: str):
     """
     Single Zone tests are the following:

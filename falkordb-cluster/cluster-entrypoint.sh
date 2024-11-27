38e380943af9ad65815adad4bfce4e415d81133e
Muhammad Qadora <memomq70@gmail.com>
2024-11-27 18:24:34 +0200

    updated

--- falkordb-cluster/cluster-entrypoint.sh
+++ falkordb-cluster/cluster-entrypoint.sh
@@ -80,6 +80,10 @@ update_ips_in_nodes_conf(){
   if [[ -f "$DATA_DIR/nodes.conf" && -s "$DATA_DIR/nodes.conf" ]];then
     res=$(cat $DATA_DIR/nodes.conf | grep myself | awk '{print $2}' | cut -d',' -f1)
     external_ip=$(getent hosts $NODE_HOST | awk '{print $1}')
+    if [[ -z $external_ip ]];then
+      echo "Could not resolve hostname, trying again: $NODE_HOST" 
+      update_ips_in_nodes_conf
+    fi
     echo "The old ip is: $res"
     echo "The new ip is: $external_ip"
     sed -i "s/$res/$external_ip:$NODE_PORT@1$NODE_PORT/" $DATA_DIR/nodes.conf

01e2afda72d7282da8e0a972a13c34a981d4e688
Dudi Zimberknopf <zimber.dudi@gmail.com>
2025-05-07 13:35:51 +0300

    update redis_exporter to 1.70

--- falkordb-node/node-entrypoint.sh
+++ falkordb-node/node-entrypoint.sh
@@ -598,7 +598,7 @@ if [[ $RUN_METRICS -eq 1 ]]; then
   # When TLS is enabled, use RANDOM_NODE_PORT to get the external port if defined (multi tenancy tiers)
   node_external_port=$(if [[ $TLS == "true" && -z $RANDOM_NODE_PORT ]]; then echo $NODE_PORT; else echo $RANDOM_NODE_PORT; fi)
   exporter_url=$(if [[ $TLS == "true" ]]; then echo "rediss://localhost:$node_external_port"; else echo "redis://localhost:$NODE_PORT"; fi)
-  redis_exporter -skip-tls-verification -redis.password $ADMIN_PASSWORD -redis.addr $exporter_url -log-format json -tls-server-min-version TLS1.3 -include-system-metrics $aof_metric_export &
+  redis_exporter -skip-tls-verification -redis.password $ADMIN_PASSWORD -redis.addr $exporter_url -log-format json -tls-server-min-version TLS1.3 -include-system-metrics -is-falkordb -slowlog-history-enabled $aof_metric_export &
   redis_exporter_pid=$!
 fi
 

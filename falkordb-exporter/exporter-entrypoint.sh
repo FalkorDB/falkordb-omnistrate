07018e9b945a08e61baa4022d16c581af49ea6a0
Dudi Zimberknopf <zimber.dudi@gmail.com>
2025-07-10 18:01:30 +0300

    fix exporter image

--- falkordb-exporter/exporter-entrypoint.sh
+++ falkordb-exporter/exporter-entrypoint.sh
@@ -1,18 +1,17 @@
-#!/bin/bash
+#!/bin/sh
 
-#ADMIN_PASSWORD=${ADMIN_PASSWORD:-''}
-if [[ -f "/run/secrets/adminpassword" ]] && [[ -s "/run/secrets/adminpassword" ]]; then
+if [ -f "/run/secrets/adminpassword" ] && [ -s "/run/secrets/adminpassword" ]; then
   ADMIN_PASSWORD=$(cat "/run/secrets/adminpassword")
   export ADMIN_PASSWORD
-elif [[ -n "$ADMIN_PASSWORD" ]]; then
+elif [ -n "$ADMIN_PASSWORD" ]; then
   export ADMIN_PASSWORD=$ADMIN_PASSWORD
 else
   export ADMIN_PASSWORD=''
 fi
 
-if [[ $RUN_METRICS -eq 1 ]]; then
+if [ -n "$RUN_METRICS" ] && [ "$RUN_METRICS" -eq "$RUN_METRICS" ] 2>/dev/null && [ "$RUN_METRICS" -eq 1 ]; then
   echo "Starting Metrics"
-  aof_metric_export=$(if [[ $PERSISTENCE_AOF_CONFIG != "no" ]]; then echo "-include-aof-file-size"; else echo ""; fi)
-  exporter_url=$(if [[ $TLS == "true" ]]; then echo "rediss://localhost:$NODE_PORT"; else echo "redis://localhost:$NODE_PORT"; fi)
+  aof_metric_export=$(if [ "$PERSISTENCE_AOF_CONFIG" != "no" ]; then echo "-include-aof-file-size"; else echo ""; fi)
+  exporter_url=$(if [ "$TLS" = "true" ]; then echo "rediss://localhost:$NODE_PORT"; else echo "redis://localhost:$NODE_PORT"; fi)
   redis_exporter -skip-tls-verification -redis.password $ADMIN_PASSWORD -redis.addr $exporter_url -log-format json -is-cluster -tls-server-min-version TLS1.3 -include-system-metrics -is-falkordb -slowlog-history-enabled $aof_metric_export
 fi

7832d4ede30155a1c19ecf99dbe13c773da26db3
Dudi Zimberknopf <zimber.dudi@gmail.com>
2025-07-15 10:48:01 +0300

    fix sentinel exporter port

--- falkordb-exporter/exporter-entrypoint.sh
+++ falkordb-exporter/exporter-entrypoint.sh
@@ -9,10 +9,25 @@ else
   export ADMIN_PASSWORD=''
 fi
 
+# get port from the first argument or use default
+if [ -n "$1" ]; then
+  redis_port=$1
+else
+  redis_port=${redis_port:-6379}
+fi
+
+# get exporter port from the second argument or use default
+if [ -n "$2" ]; then
+  exporter_port=$2
+else
+  exporter_port=${exporter_port:-9121}
+fi
+
 if [ -n "$RUN_METRICS" ] && [ "$RUN_METRICS" -eq "$RUN_METRICS" ] 2>/dev/null && [ "$RUN_METRICS" -eq 1 ]; then
   echo "Starting Metrics"
   aof_metric_export=$(if [ "$PERSISTENCE_AOF_CONFIG" != "no" ]; then echo "-include-aof-file-size"; else echo ""; fi)
-  exporter_url=$(if [ "$TLS" = "true" ]; then echo "rediss://localhost:$NODE_PORT"; else echo "redis://localhost:$NODE_PORT"; fi)
+  redis_url=$(if [ "$TLS" = "true" ]; then echo "rediss://localhost:$redis_port"; else echo "redis://localhost:$redis_port"; fi)
+  exporter_address="0.0.0.0:$exporter_port"
   # shellcheck disable=SC2068
-  redis_exporter -skip-tls-verification -redis.password $ADMIN_PASSWORD -redis.addr $exporter_url -log-format json -tls-server-min-version TLS1.3 -include-system-metrics -is-falkordb -slowlog-history-enabled $aof_metric_export $@
+  redis_exporter -skip-tls-verification -redis.password $ADMIN_PASSWORD -redis.addr $redis_url -web.listen-address $exporter_address -log-format json -tls-server-min-version TLS1.3 -include-system-metrics -is-falkordb -slowlog-history-enabled $aof_metric_export $@
 fi
